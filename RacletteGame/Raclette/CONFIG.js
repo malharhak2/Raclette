438201045c9df882b1c5068ce65ea0b575790072
Johan Lajili <johan.lajili@gmail.com>
2013-04-14 02:52:34 +0200

    correction de bugs / 3C

--- RacletteGame/Raclette/CONFIG.js
+++ RacletteGame/Raclette/CONFIG.js
@@ -24,7 +24,7 @@ define(["game/config"], function (userConfig) {
 		this.gameTitle = userConfig.gameTitle || "UNTITLED";
 		this.containerID = userConfig.gameContainer || "#racletteGame";
 		this.guiID = userConfig.guiID || "#racletteGui";
-		this.backgroundColor = userConfig.backgroundColor || "#A099A0";
+		this.backgroundColor = userConfig.backgroundColor || "#464F63";
 		this.screen = userConfig.screen || {
 			mode : userConfig.screen.mode || "ratio_css",
 			ratio : userConfig.screen.ratio || 14/10,
@@ -33,7 +33,7 @@ define(["game/config"], function (userConfig) {
 		};
 		this.pressedThreshold = userConfig.pressedThreshold || 0.5;
 		this.floor = 62;
-		this.canvasBackgroundColor = userConfig.canvasBakgroundColor || "#4E3D61";
+		this.canvasBackgroundColor = userConfig.canvasBackgroundColor || "#464F63";
 		this.showCredit = userConfig.showCredit || true;
 		this.showCustomFooter = userConfig.showCustomFooter || true;
 		this.customFooter = userConfig.customFooter || "Raclette engine inside";
